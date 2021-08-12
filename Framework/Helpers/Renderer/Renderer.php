173b92173a9cef9e1d788d188a13a16ce0be4295
Yehor Hrytsenko <egor.grits56@gmail.com>
2021-08-12 23:57:29 +0300

    Add class Renderer

---
+++ Framework/Helpers/Renderer/Renderer.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Framework\Helpers\Render;
+
+class Renderer
+{
+    private const PATH_TO_VIEW = "App/View/";
+
+    public function render($layout, $params, $template): bool
+    {
+        ob_start();
+        try {
+            $template = self::PATH_TO_VIEW . 'Templates/' . $template;
+            include(self::PATH_TO_VIEW . 'Layouts/' . $layout);
+            ob_flush();
+            return ob_end_clean();
+        } catch (\Exception $e) {
+            ob_end_clean();
+            throw $e;
+        }
+    }
+}
