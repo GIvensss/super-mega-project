09df7ad4e1d98cb24ed8a94857e3b19fc9650afd
Yehor Hrytsenko <egor.grits56@gmail.com>
2021-08-23 17:59:33 +0300

    Implement pattern Singletone in Database

--- Framework/Database/Database.php
+++ Framework/Database/Database.php
@@ -6,28 +6,25 @@ use PDO;
 
 class Database
 {
+    private static $instance;
+
     private static ?PDO $db = null;
     private array $dbparams;
+
 
     public function __construct()
     {
         $this->dbparams = require('App/Config/dbparams.php');
-    }
 
-    public function connect(): PDO
-    {
-        if (!self::$db) {
-            $connection = $this->dbparams['DB_CONNECTION'];
-            $host = $this->dbparams['DB_HOST'];
-            $dbname = $this->dbparams['DB_DATABASE'];
-            $dsn = $connection . ':dbname=' . $dbname . ';host=' . $host;
+        $connection = $this->dbparams['DB_CONNECTION'];
+        $host = $this->dbparams['DB_HOST'];
+        $dbname = $this->dbparams['DB_DATABASE'];
+        $dsn = $connection . ':dbname=' . $dbname . ';host=' . $host;
 
-            $user = $this->dbparams['DB_USERNAME'];
-            $password = $this->dbparams['DB_PASSWORD'];
+        $user = $this->dbparams['DB_USERNAME'];
+        $password = $this->dbparams['DB_PASSWORD'];
 
-            self::$db = new PDO($dsn, $user, $password);
-        }
-        return self::$db;
+        self::$db = new PDO($dsn, $user, $password);
     }
 
     public function query(string $query, array $params = [])
@@ -60,5 +57,12 @@ class Database
     {
         self::$db->rollBack();
     }
+    public static function getInstance(): self
+    {
+        if (self::$instance === null) {
+            self::$instance = new self();
+        }
 
+        return self::$instance;
+    }
 }
