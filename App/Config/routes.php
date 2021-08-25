2dd140ef92745b713d38837eb888110438fbfa60
Yehor Hrytsenko <egor.grits56@gmail.com>
2021-08-25 11:06:29 +0300

    Update routes for Router

--- App/Config/routes.php
+++ App/Config/routes.php
@@ -4,19 +4,24 @@ namespace Framework\Router;
 
 return [
 
+    'cabinet/orders' => '/cabinet/showOrders',
     'cabinet' => '/cabinet/index',
+
+    'cart' => '/cart/index',
 
     'registration' => '/registration/index',
 
     'auth/logout' => '/authentication/logout',
     'auth' => '/authentication/index',
 
-    'products/get' => "/products/getProducts",
+    'products/page/([0-9]+)' => '/products/page/$1',
     'products/([0-9]+)' => '/products/showItem/$1',
-    'products' => '/products/showList',
 
+    'api/get/page/([0-9]+)' => '/api/getPage/$1',
+    'api/get/total-page' => '/api/getTotalPages/',
     'api/get/([0-9]+)' => '/api/getProductById/$1',
     'api/get' => '/api/getProducts',
+    'api/post-order' => '/api/postOrder',
 
     '' => '/products/index'
 ];
