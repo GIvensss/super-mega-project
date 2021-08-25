eb6d69a53515f0551673c7073bb36f6fdc8d81b1
Yehor Hrytsenko <egor.grits56@gmail.com>
2021-08-25 23:36:04 +0300

    Update searching panel

--- App/View/Layouts/header.php
+++ App/View/Layouts/header.php
@@ -24,54 +24,57 @@
 </head>
 <body>
 <header>
-    <div class="ceiling">
-        <div class="row">
-            <div class="col-md-4">
-        <a href="/">
-            <img class="main-icon" src="../../../src/images/rock-symbol.svg" alt="rock-ico">
-        </a>
+    <div class="container">
+        <div class="ceiling">
+            <div class="row">
+                <div class="col-md-4">
+                    <a href="/">
+                        <img class="main-icon" src="../../../src/images/rock-symbol.svg" alt="rock-ico">
+                    </a>
+                </div>
+                <div class="col-md-4 offset-md-4">
+                    <a href="/cart">
+                        <img class="basket-icon" src="../../../src/images/basket.svg" alt="basket">
+                        <span class="badge bg-secondary mx-2 cartProductAmount">
+                            0
+                        </span>
+                    </a>
+                    <?php if (isset($_SESSION['username'])) : ?>
+                        <a href="/cabinet/index">
+                            <img class="cabinet-icon" src="../../../src/images/punk.png" alt="cabinet">
+                        </a>
+                        <a href="/auth/logout">
+                            <img class="logout-icon" src="../../../src/images/log-out.png" alt="logout">
+                        </a>
+                    <?php else : ?>
+                        <a href="/auth">
+                            <img class="cabinet-icon" src="../../../src/images/punk.png" alt="cabinet">
+                        </a>
+                    <?php endif; ?>
+                </div>
             </div>
-            <div class="col-md-4 offset-md-4">
-                <a href="/cart">
-                    <img class="basket-icon" src="../../../src/images/basket.svg" alt="basket">
-                    <span class="badge bg-secondary mx-2 cartProductAmount">
-                        0
-                    </span>
-                </a>
-                <?php if (isset($_SESSION['username'])) : ?>
-                    <a href="/cabinet/index">
-                        <img class="cabinet-icon" src="../../../src/images/punk.png" alt="cabinet">
-                    </a>
-                    <a href="/auth/logout">
-                        <img class="logout-icon" src="../../../src/images/log-out.png" alt="logout">
-                    </a>
-                <?php else : ?>
-                    <a href="/auth">
-                        <img class="cabinet-icon" src="../../../src/images/punk.png" alt="cabinet">
-                    </a>
-                <?php endif; ?>
-
-            </div>
+            <form action="/search" method="post">
+                <div class="row">
+                        <div class="col-md-3 offset-md-3">
+                            <input type="search" class="search" name="q" placeholder="Search">
+                        </div>
+                        <div class="col-md-2">
+                            <button type="submit" class="search" value="Search">Search</button>
+                        </div>
+                </div>
+            </form>
         </div>
         <div class="row">
-                <div class="col-md-4 offset-md-2">
-                    <input type="search" class="search" name="q" placeholder="Search">
+            <nav>
+                <div class="menu">
+                    <ol class="menu">
+                        <li>Categories</li>
+                        <li>Bands</li>
+                        <li>About</li>
+                        <li>Feedback</li>
+                    </ol>
                 </div>
-                <div class="col-md-2">
-                    <button type="submit" class="search" value="Search">Search</button>
-                </div>
+            </nav>
         </div>
-    </div>
-    <div class="row">
-        <nav>
-            <div class="menu">
-                <ol class="menu">
-                    <li>Categories</li>
-                    <li>Bands</li>
-                    <li>About</li>
-                    <li>Feedback</li>
-                </ol>
-            </div>
-        </nav>
     </div>
 </header>
