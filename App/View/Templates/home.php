dad1e37a60d509f17bd47427977c05e10495e64b
Yehor Hrytsenko <egor.grits56@gmail.com>
2021-08-25 11:03:59 +0300

    Update templates

--- App/View/Templates/home.php
+++ App/View/Templates/home.php
@@ -1,14 +1,16 @@
 <main>
     <div class="main">
         <section>
-            <div class="interesting">
-                <img src="../../../src/images/banner1.jpg">
-                <h1>Store opening. Discounts up to 30%</h1>
-                <p>
-                    In honor of the opening of the store,
-                    we offer you discounts on goods from
-                    the T-shirts, bracelets, rings
-                </p>
+            <div class="container">
+                <div class="interesting">
+                    <img src="../../../src/images/banner1.jpg">
+                    <h1>Store opening. Discounts up to 30%</h1>
+                    <p>
+                        In honor of the opening of the store,
+                        we offer you discounts on goods from
+                        the T-shirts, bracelets, rings
+                    </p>
+                </div>
             </div>
         </section>
         <section>
@@ -21,6 +23,13 @@
                         </div>
                     </div>
                 </div>
+            </div>
+            <div class="d-flex justify-content-center">
+                <h3>
+                    <a href="/products/page/1">
+                        Show More
+                    </a>
+                </h3>
             </div>
         </section>
     </div>
--- App/View/Templates/products.php
+++ App/View/Templates/products.php
@@ -1,41 +1,29 @@
 <main>
+    <div class="container">
     <div class="main">
         <section>
             <div class="main-cards">
                 <div class="col-lg-02 col-md-3">
                     <div class="top-cover center-block">
-                        <!-- PLACE FOR PRODUCTS -->
+                            <!-- PLACE FOR PRODUCTS -->
                         <img class="top-name center-block text-center"
-                             src="../../../src/images/waiting.gif" alt="loading...">
+                                 src="../../../src/images/waiting.gif" alt="loading...">
                     </div>
                 </div>
             </div>
         </section>
     </div>
-    <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalCenterTitle">Modal title</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    ...
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
-                </div>
-            </div>
-        </div>
+    <div class="row">
+        <ul class="pagination justify-content-center">
+        </ul>
     </div>
 </main>
+<script src="../../../src/js/url.js"></script>
 <script src="../../../src/js/request.js"></script>
+<script src="../../../src/js/pages.js"></script>
 <script src="../../../src/js/products.js"></script>
 <script src="../../../src/js/cart.js"></script>
-<script src="../../../src/js/main.js"></script>
+<script src="../../../src/js/productsMain.js"></script>
 
 <!--                <div class="cards popular">-->
 <!--                    <h3>Popular: be always in trend</h3><br>-->
