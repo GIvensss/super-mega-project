eccec1fda7d945a15db823f7321567549e40657d
Yehor Hrytsenko <egor.grits56@gmail.com>
2021-08-27 02:46:29 +0300

    Add new routes

--- App/Config/routes.php
+++ App/Config/routes.php
@@ -14,11 +14,20 @@ return [
     'auth/logout' => '/authentication/logout',
     'auth' => '/authentication/index',
 
+
     'products/page/([0-9]+)' => '/products/page/$1',
+    'products/category/([0-9]+)' => '/products/category/$1',
+    'products/band/([0-9]+)' => '/products/band/$1',
     'products/([0-9]+)' => '/products/showItem/$1',
+    'products' => '/products/index',
+
+
+    'search' => '/search/index',
 
     'api/get/page/([0-9]+)' => '/api/getPage/$1',
     'api/get/total-page' => '/api/getTotalPages/',
+    'api/get/categories' => '/api/getCategories',
+    'api/get/bands' => '/api/getBands',
     'api/get/([0-9]+)' => '/api/getProductById/$1',
     'api/get' => '/api/getProducts',
     'api/post-order' => '/api/postOrder',
